d507ff1548232f03f01a1064f6873f4392c97c47
dheim123 <dheim@midland.k12.ia.us>
2025-10-15 10:58:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
-var quotelength = quote.length;
+var length = quote.length;
 var twentySecondLetter = quote[21];
 var output = twentySecondLetter;
 
